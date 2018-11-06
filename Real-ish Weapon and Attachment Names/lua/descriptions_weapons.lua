40099e89d6053a7f3627269298e01ab5ab0fbecc
VxWolf <vxwolf368@gmail.com>
2018-11-06 13:51:24 +1100

    Delete descriptions_weapons.lua

--- Real-ish Weapon and Attachment Names/lua/descriptions_weapons.lua
+++
@@ -1,7 +0,0 @@
-Hooks:PostHook(WeaponTweakData, "init", "Real-ish Weapon Names", function(self)
-    for _, weapon in pairs(self) do
-        if weapon.desc_id then --this is necessary apparently
-            weapon.has_description = true
-        end
-    end
-end)
