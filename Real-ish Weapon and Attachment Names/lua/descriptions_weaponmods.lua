8547c2a5ca5a232273b85656355b0cc2c4dd5d19
VxWolf <vxwolf368@gmail.com>
2018-11-06 13:51:29 +1100

    Delete descriptions_weaponmods.lua

--- Real-ish Weapon and Attachment Names/lua/descriptions_weaponmods.lua
+++
@@ -1,7 +0,0 @@
-Hooks:PostHook(WeaponFactoryTweakData, "init", "Real-ish Weapon Names", function(self)
-    for _, parts in pairs(self.parts) do
-        --if parts.desc_id then  -- <-- unused bit. this check is unneccessary apparently.
-            parts.has_description = true
-        --end
-    end
-end)
