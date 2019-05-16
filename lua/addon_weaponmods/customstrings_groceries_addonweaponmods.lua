4330766552d68c2627f329d42c24a2e59257bc3b
VxWolf <vxwolf368@gmail.com>
2019-05-16 17:26:39 +1000

    Groceries attachments
    Sneaky Suppressor Pack

---
+++ lua/addon_weaponmods/customstrings_groceries_addonweaponmods.lua
@@ -0,0 +1,16 @@
+Hooks:Add("LocalizationManagerPostInit", "RishWN_Groceries_AddonAttachments", function(loc)
+	LocalizationManager:add_localized_strings({
+		
+		--Sneaky Suppressor Pack
+		bm_wp_wpn_fps_ass_ns_g_sup1 = "DIAgeM Taiga",
+		bm_wp_wpn_fps_ass_ns_g_sup2 = "Tecci Dik-tator",
+		bm_wp_wpn_fps_ass_ns_g_sup3 = "Bowser v200",
+		bm_wp_wpn_fps_ass_ns_g_sup4 = "BeSilent! Banshee",
+		bm_wp_wpn_fps_ass_ns_g_sup5 = "DIAgeM Stalker",
+		bm_wp_wpn_fps_ass_ns_g_sup6 = "Goats' Frontline Poseidon",
+		bm_wp_wpn_fps_ass_ns_g_sup7 = "Raven Hunter Low-weight",
+		bm_wp_wpn_fps_ass_ns_g_sup8 = "BeSilent! Snugbullet",
+		
+		thislineisadummystringso = "I don't have to worry about adding/removing" -- a comma every time i add an attachment
+	})
+end)
