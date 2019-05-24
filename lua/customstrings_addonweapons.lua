320f2c547769d9d12e0d706016af3c54676988fa
Alcat101 <adhanika15@gmail.com>
2019-05-24 18:50:04 +0700

    Other customweapon
    Descrip tweak to Style Guide
    
    PLA to Chicom
    Added more Silent Enforcer PLA gun
    Added Ellie PLA gun

--- lua/customstrings_addonweapons.lua
+++ lua/customstrings_addonweapons.lua
@@ -3,34 +3,46 @@ Hooks:Add("LocalizationManagerPostInit", "RishWN_AddonGuns", function(loc)
 
 		--Killerwolf
 		bm_w_dp28 = "Tachanka LT28", --DP28
-		bm_w_dp28_desc = "Old WWII Russian LMG. Extreme Damage - Great part variety.",
+		bm_w_dp28_desc = "Old WWII Russian LMG. Extreme damage - Wide part variety.",
 
 
 		--Carl
 		bm_w_nya = "ParkHale IDW", --made by Parker-Hale
-		bm_w_nya_desc = "Gained traction as a submachine gun featured on some mobile phone game..? Moderate Damage - High rate of fire.",
+		bm_w_nya_desc = "Gained traction as a submachine gun featured on some mobile phone game..? Moderate damage - High rate of fire.",
 		bm_w_x_nya = "Akimbo ParkHale IDW",
 		bm_w_x_nya_desc = "Wield two ParkHale IDWs simultaneously in akimbo. Make Hayao Miyazaki disappointed.",
 
 
 		--Matthelzor
 		bm_w_yayo = "Crosskill AMR-16 'Montana'", --Montana 5.56
-		bm_w_yayo_desc = "The infamous assault rifle of an infamous drug lord. Moderate Damage - Has an underbarrel grenade launcher.",
+		bm_w_yayo_desc = "The infamous assault rifle of an infamous drug lord. Moderate damage - Has an underbarrel grenade launcher.",
     
     
 		--Silent Enforcer
 		bm_w_toz194 = "IZHMA Tulip 194",  --Tula Arms are now under the IZHMA conglomerate, Tula is now TOZ shotgun brand line
-		bm_w_toz194_desc = "Exceptionally long-barreled shotgun made to pass Russian civilian firearm ownership laws. Moderate Damage - Low ammo capacity.",
+		bm_w_toz194_desc = "Exceptionally long-barreled shotgun made to pass Russian civilian firearm ownership laws. Moderate damage - Low ammo capacity.",
 		
-		bm_w_qbs = "CHIWEP CQS-09", --Norinco QBS-09
-		bm_w_qbs_desc = "Semi-automatic light shotgun adopted by Chinese armies. Low Damage - High concealment.",
+		bm_w_qbs = "Chicom QSho-9", --Norinco QBS-09, Sho for Shotgun
+		bm_w_qbs_desc = "Semi-automatic light shotgun adopted by Chinese armies. Low damage - High concealment.",
+		
+		bm_w_qbz3 = "Chicom QArM-3", --QBZ-03, ArM for AR Modern. The probable evolution of bullpup QBZ 95 to conventional mag-front-trigger form
+		bm_w_qbz3_desc = "Touted to be the next evolution of Chinese Assault Rifle. Moderate damage - Balanced performance.",
+		
+		bm_w_qbz95 = "Chicom QAr-95", --QBZ95, Ar for assault rifle
+		bm_w_qbz95_desc = "The mainline assault rifle of Chinese armies. Moderate damage - Decent part variety.",
+		
+		
+		--Ellie
+		bm_w_fyjs = "Chicom FYJS", --included for PLA pack completeness
+		bm_w_fyjs_desc = "Presicion Chinese military sniper rifle. Low damage - Balanced performance.",
+				
 		
 		--VxWolf        
 		bm_w_temple = "SG Hyper11", --G11
-		bm_w_temple_desc = "Rare prototype German carbine. Low Damage - Has a rapid-fire hyperburst function.",
+		bm_w_temple_desc = "Rare prototype German carbine. Low damage - Has a rapid-fire hyperburst function.",
             
 		bm_w_fortress = "eDEN Bavarium Splitter",   --Bavarium Splitter
-		bm_w_fortress_desc = "An assault rifle that combines its ammunition with a highly reactive material. High Damage - High magazine size.",
+		bm_w_fortress_desc = "An assault rifle that combines its ammunition with a highly reactive material. High damage - Large magazine.",
 
 		
 		--xdDad
