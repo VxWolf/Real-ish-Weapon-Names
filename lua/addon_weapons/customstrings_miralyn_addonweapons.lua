32a048f8f798c9143d799b3d3d2a52f80434d3b0
Miralyn <41351810+Miralyn@users.noreply.github.com>
2019-09-14 23:24:18 +1000

    Create customstrings_miralyn_addonweapons.lua

---
+++ lua/addon_weapons/customstrings_miralyn_addonweapons.lua
@@ -0,0 +1,70 @@
+Hooks:Add("LocalizationManagerPostInit", "RishWN_Mira_AddonGuns", function(loc)
+    LocalizationManager:add_localized_strings({
+
+        --Miralyn
+        bm_w_nightgoddess = "IZHMA Valkyria MOD.3", --AS Val MOD.3
+        bm_w_nightgoddess_desc = "Custom-built version of the Valkyria rifle. High damage - Integrally suppressed.",
+        
+		    bm_w_axewscope = "CR 75 TS Czechmate", --CZ 75 TS Czechmate
+        bm_w_axewscope_desc = "IPSC-ready version of a famous Czech pistol. High damage - Large magazines.",
+
+		    bm_w_bigbust = "GSR-Precisions GSR-1", --DSR-1
+        bm_w_bigbust_desc = "Bullpup sniper rifle. High damage - Great accuracy.",
+
+		    bm_w_grotb = "Radon MFS-5.56B", --MSBS-5.56B
+        bm_w_grotb_desc = "Bullpup variant of the Polish assault rifle. Low damage - High concealment.",
+
+	    	bm_w_fang45 = "MWR Fang-45", --Fang-45
+        bm_w_fang45_desc = "Custom-built SMG based on the CAR platform. Moderate ammo capacity - Poor concealment",
+
+	    	bm_w_fnp45 = "UN P45T", --FN FNP-45
+        bm_w_fnp45_desc = "Belgian pistol chambered in .45 ACP. Moderate damage - Capable of automatic fire.",
+
+		    bm_w_hk33 = "SG SG33", --HK33
+        bm_w_hk33_desc = "German 5.56mm rifle derived from the Gewehr 3 rifle series. Moderate damage - Poor concealment.",
+
+	    	bm_w_fpsix = "Foburme FPSix", --FP6
+        bm_w_fpsix_desc = "Sleek Italian shotgun. High damage - Good concealment",
+
+	    	bm_w_mp7new = "SG Compact-7", --MP7
+        bm_w_mp7new_desc = "Older version of the SpecOps SMG. Penetrates body armor - Good Concealment.",
+
+		    bm_w_vp70 = "SG LSK-70M", -- VP70
+        bm_w_vp70_desc = "Military version of a rare German pistol. Moderate damage - Has a burst fire function",
+		    bm_w_x_vp70 = "Akimbo SG LSK-70Z", --Akimbo VP70
+        bm_w_x_vp70_desc = "Wield two LSC-70s simultaneously in akimbo. Make Leon Scott Kennedy envy you.",
+
+	    	bm_w_wargoddess = "Moonbase Armory Mk14", --Mk 14 EBR
+        bm_w_wargoddess_desc = "A unique mix between the AR and SR. Sniper rifle level of Armor Penetration - High RoF.",
+
+	    	bm_w_t1000x5 = "WepSys T5K", --ORSIS T-5000 |ORSIS literally stands for Weapon Systems
+        bm_w_t1000x5_desc = "Russian high-precision sniper rifle. Great accuracy - Large magazines.",
+
+		    bm_w_shieldgirl = "Sandy Tech MDR-C", --MDR-C
+        bm_w_shieldgirl_desc = "Compact version of the American bullpup rifle. Good concealment - High ammo capacity",
+
+		    bm_w_op68 = "Reinfeld ACR-CQB", --ACR-C
+        bm_w_op68_desc = "Compact version of the unsuccesful CAR-4 replacement. Balanced performance - High ammo capacity",
+        
+    		bm_w_qbz97b = "Chicom QAr-97B", --QBZ-97B
+        bm_w_qbz97b_desc = "Compact version of the export version of the Chinese AR. Moderate damage - Good concealment.",
+
+		    bm_w_saigry = "Striking Armory WRY", --SAI GRY |JoJokes, even though I didn't watch it
+        bm_w_saigry_desc = "Heavily customized member of the CAR family. High damage - Poor concealment.",
+
+		    bm_w_tkpd = "IZHMA GHSC Storm", --TKPD Storm
+        bm_w_tkpd_desc = "Custom-built rifle based on the large-caliber variant of Grom. Extreme damage - Moderate ammo return.",
+    
+        bm_w_b42 = "Wels Recon Tactical", --Steyr Elite
+        bm_w_b42_desc = "Relatively compact bolt-action rifle in traditional configuration. Moderate damage - High ammo return.",
+        
+		    bm_w_tar21 = "IMI Tara 5.56", --TAR-21
+        bm_w_tar21_desc = "Full-sized variant of the Tempest-21 AR. Balanced stats - Good concealment.",
+
+		    bm_w_hugsforleon = "Tecci CL-3", --TR-1
+        bm_w_hugsforleon_desc = "Wick's rifle of choice. Semi-auto only - Higher stats compared to other ARs",
+        
+
+        thisaswellisadummystringso = "I don't have to worry about adding/removing" -- a comma every time i add a gun
+    })
+end)
